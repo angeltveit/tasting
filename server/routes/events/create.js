8dd87ca8b8f31240c6e94db4099b3e0eb38865dd
angeltveit <=>
2018-10-12 21:42:53 +0200

    create event endpoint

---
+++ server/routes/events/create.js
@@ -0,0 +1,26 @@
+const Joi = require('joi')
+const randomString = require('random-string');
+const db = require('../../services/db')
+
+const schema = Joi.object().keys({
+  name: Joi.string().min(3).max(30).required(),
+})
+
+module.exports = async function create_event(req, res) {
+  let result = Joi.validate(req.body, schema)
+
+  if(result.error) return res.status(500).json({
+    error: result.error.details[0].message
+  })
+
+  const code = randomString({ length: 8 })
+  console.log(req.body)
+  req.body = Object.assign({}, req.body, {
+    code,
+  })
+
+  const event = await db('events').insert(req.body)
+    .returning(['id', 'name', 'code', 'created_at'])
+    
+  res.status(200).json(Object.assign({ status: 'ok' }, event[0]))
+}
