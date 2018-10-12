ddbdace7df5df2fff155186834c3e083b1af0bf2
angeltveit <=>
2018-10-12 20:05:20 +0200

    knex migrations

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 const {parse} = require('pg-connection-string')
-const { database } = require('server/config')
+const { database } = require('./server/config')
 module.exports = {
   client: 'pg',
   connection: parse(database || 'postgres://localhost/tasting'),
